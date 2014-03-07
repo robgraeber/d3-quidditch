d69da6add77dad5e3b36f15fe6e7c6e93b2faafe
Rob Graeber <bgraeber@gmail.com>
2014-03-07 12:10:12 -0800

    Setup the player

--- watchout.js
+++ watchout.js
@@ -28,3 +28,86 @@ var updateBestScore = function(){
   gameStats.bestScore = _.max(gameStats.bestScore, gameStats.score);
   d3.select('#best-score').text(gameStats.bestScore.toString());
 };
+
+var Player = function(gameOptions){
+  this.gameOptions = gameOptions;
+  var path = 'm-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z';
+  var fill = '#ff6600';
+  var x = 0;
+  var y = 0;
+  var angle = 0;
+  var r = 5;
+  var that = this;
+  //adds to svg and setup default position
+  this.render = function(to) {
+    this.el = to.append('svg:path')
+                .attr('d', path)
+                .attr('fill', fill);
+    this.transform({
+      x: this.gameOptions.width * 0.5,
+      y: this.gameOptions.height * 0.5
+    });
+
+    this.setupDragging();
+    // this;
+  };
+  this.getX = function(){
+    return x;
+  };
+  this.setX = function(num){
+    minX = this.gameOptions.padding;
+    maxX = this.gameOptions.width - this.gameOptions.padding;
+    num = Math.max(minX, num);
+    num = Math.min(maxX, num);
+    x = num;
+  };
+  this.getY = function(){
+    return y;
+  };
+  this.setY = function(num){
+    minY = this.gameOptions.padding;
+    maxY = this.gameOptions.height - this.gameOptions.padding;
+    num = Math.max(minY, num);
+    num = Math.min(maxY, num);
+    y = num;
+  };
+  this.transform = function(opts){
+    angle = opts.angle || angle;
+    console.log(opts.x, opts.y);
+    this.setX(opts.x || x);
+    this.setY(opts.y || y);
+    console.log(this.getX(), this.getY());
+    this.el.attr('transform', "translate("+this.getX()+","+this.getY()+")");
+      // "rotate("+this.angle+","+this.getX()+","+this.getY()+")"+
+
+    console.log('transform', "translate("+this.getX()+","+this.getY()+")");
+  };
+  this.moveAbsolute = function(x,y){
+    this.transform({x:x, y:y});
+  };
+
+  this.moveRelative = function (dx,dy){
+    this.transform({
+      x: this.getX()+dx,
+      y: this.getY()+dy,
+      angle: 360 * (Math.atan2(dy,dx)/(Math.PI*2))
+    });
+  };
+
+  this.setupDragging = function(){
+    var dragMove = function(){
+      that.moveRelative(d3.event.dx, d3.event.dy);
+    };
+    var drag = d3.behavior.drag()
+                          .on('drag', dragMove);
+    this.el.call(drag);
+  };
+};
+var players = [];
+players.push(new Player(gameOptions));
+for (var i = 0; i < players.length; i++){
+  players[i].render(gameBoard);
+}
+
+
+
