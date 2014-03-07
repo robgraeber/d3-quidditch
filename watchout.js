88b9b68696be8869aab87114f24deb7967062ee0
Rob Graeber <bgraeber@gmail.com>
2014-03-07 11:11:35 -0800

    Setup the game board

--- watchout.js
+++ watchout.js
@@ -1,13 +1,30 @@
 // start slingin' some d3 here.
 
-gameOptions = {
+var gameOptions = {
   height: 450,
   width: 700,
   nEnemies: 30,
   padding: 20
 };
 
-gameStats = {
+var gameStats = {
   score: 0,
   bestScore: 0
+};
+
+var axes = {
+  x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
+  y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
+};
+var gameBoard = d3.select('.container').append('svg:svg')
+                                       .attr('width', gameOptions.width)
+                                       .attr('height', gameOptions.height);
+
+var updateScore = function(){
+  d3.select('#current-score')
+    .text(gameStats.score.toString());
+};
+var updateBestScore = function(){
+  gameStats.bestScore = _.max(gameStats.bestScore, gameStats.score);
+  d3.select('#best-score').text(gameStats.bestScore.toString());
 };
